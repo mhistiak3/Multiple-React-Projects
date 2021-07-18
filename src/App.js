1aebf3dd6053d89144af2eda1b6b72d26feaa1a2
Istiak <mhistiak2@gmail.com>
2021-07-18 20:51:01 +0600

    Fresh Project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./component/layout/Navbar";
-import About from "./component/pages/About";
-import Contact from "./component/pages/Contact";
-import Home from "./component/pages/Home";
-import NotFound from "./component/pages/NotFound";
-import AddUser from "./component/users/AddUser";
-import EditUser from "./component/users/EditUser";
-import ViweUser from "./component/users/ViweUser";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/user/add" component={AddUser} />
-        <Route exact path="/user/edit/:id" component={EditUser} />
-        <Route exact path="/user/viwe/:id" component={ViweUser} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+   <div>
+     Hello
+   </div>
   );
 }
 
